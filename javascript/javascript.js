e16e2ca191d497cfc7ef07975157098310b20974
Min Naing Oo <minnaingoo792@gmail.com>
2026-03-23 01:53:45 +0630

    links-unfinish

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,8 +1,19 @@
+// const menubar = document.getElementById('menu-bar');
+// const linkContainer = document.querySelector('.link_container');
+
+// if (menubar) {
+//     menubar.addEventListener('click', function() {
+//         console.log("Menu bar clicked!"); // Console မှာ ပေါ်မပေါ် ကြည့်ပါ
+//         linkContainer.classList.toggle('show');
+//     });
+// }
+
 const menubar = document.getElementById('menu-bar');
 const linkContainer = document.querySelector('.link_container');
 
-    menubar.onclick = function (){
+if (menubar && linkContainer) {
+    menubar.onclick = function() {
         linkContainer.classList.toggle('show');
-        console.log("Menu Clicked!");
-        alert("Js connect");
-    }
+        console.log("Menu toggled!"); 
+    };
+}
